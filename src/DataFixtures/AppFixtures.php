e8d6e7710326c81395b2f2ea43bf497455ff126b
Wiliam64 <lemasson.wiliam@gmail.com>
2020-04-24 10:36:20 +0200

    INtégration du template sur la page login

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,73 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\TypeEquipments;
+use App\Entity\Customers;
+use App\Entity\Projects;
+use App\Entity\Actions;
+use App\Entity\Conditions;
+use App\Entity\Attributes;
+use App\Entity\States;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Fixtures type d'équipements
+        $TypeEquipment = new TypeEquipments();
+        $TypeEquipment->setTitle("1 type");
+        $TypeEquipment->setDescription("Lorem Ipsum");
+        $manager->persist($TypeEquipment);
+        $manager->flush();
 
+        // Fixtures Clients
+        $Customers = new Customers();
+        $Customers->setName("Client 1");
+        $Customers->setCompany("Simplon");
+        $manager->persist($Customers);
+        $manager->flush();
+
+        //Fixtures Projets
+        $Projects = new Projects();
+        $Projects->setName("Alpha");
+        $Projects->setDescription("Lorem Ipsum");
+        $Projects->getUsers(1);
+        $Projects->getCustomers(1);
+        $Projects->getEquipments(1);
+        $manager->persist($Projects);
+        $manager->flush();
+
+        //Fixtures Actions
+        $Actions = new Actions();
+        $Actions->setKeyAction("ouvrir");
+        $Actions->setValue("ouvre la vanne");
+        $manager->persist($Actions);
+        $manager->flush();
+
+        //Fixtures Conditions
+        $Conditions = new Conditions();
+        $Conditions->setKeyCondition("conditions");
+        $Conditions->setValue("si...alors");
+        $manager->persist($Conditions);
+        $manager->flush();
+
+        //Fixtures Attributes
+        $Attributes = new Attributes();
+        $Attributes->setKeyAttribute("couleur");
+        $Attributes->setValue("bleue");
+        $manager->persist($Attributes);
+        $manager->flush();
+
+        //Fixtures States
+        $States = new States();
+        $States->getTypeEquipments();
+        $States->setTypeEquipments($TypeEquipment);
+        $States->setName("State 1");
+        $States->setDescription("Description du state 1 ");
+        $States->getActions(1);
+        $States->getConditions(1);
+        $States->getAttributes(1);
+        $manager->persist($States);
         $manager->flush();
     }
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -18,10 +18,22 @@ class UserFixtures extends Fixture
     
     public function load(ObjectManager $manager)
     {
+        //Admin
         $User = new Users();
         $User->setName("Toto");
         $User->setEmail("Toto@test.com");
         $User->setRoles( array_unique( ['ROLE_ADMIN']) );
+        $User->setFirstName("Toto");
+        $password = $this->passwordEncoder->encodePassword($User, 'admin');
+        $User->setPassword($password);        
+        $manager->persist($User);
+        $manager->flush();
+
+        //User
+        $User = new Users();
+        $User->setName("Toto");
+        $User->setEmail("Test@test.com");
+        $User->setRoles( array_unique( ['ROLE_USER']) );
         $User->setFirstName("Toto");
         $password = $this->passwordEncoder->encodePassword($User, 'admin');
         $User->setPassword($password);        
